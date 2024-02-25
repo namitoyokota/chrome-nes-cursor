f6a71c56790171767f57b09d36bbb3a4e4dd75b2
Namito Yokota <namitoyokota@icloud.com>
2024-02-24 21:19:16 -0600

    comments to background script

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
+// Listens to extension icon click
 chrome.browserAction.onClicked.addListener(() => {
     chrome.storage.local.get('enabled', (data) => {
         chrome.storage.local.set({ enabled: !data.enabled });
     });
 });
 
+// Listens to navigation page
 chrome.webNavigation.onHistoryStateUpdated.addListener(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const currentTab = tabs[0];
