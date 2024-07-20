0614a1957212fc3e0b7c55d61a7d965972352989
Namito Yokota <namitoyokota@icloud.com>
2024-07-20 00:28:13 -0500

    Set on/off badge.

--- background.js
+++ background.js
@@ -2,5 +2,8 @@
 chrome.action.onClicked.addListener(() => {
     chrome.storage.local.get('enabled', (data) => {
         chrome.storage.local.set({ enabled: !data.enabled });
+        chrome.action.setBadgeText({
+            text: data.enabled ? 'OFF' : 'ON',
+        });
     });
 });
